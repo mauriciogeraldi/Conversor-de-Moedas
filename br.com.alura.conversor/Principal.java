20fb9d539e0ec9540aed7330e2efbb9d48c42ed4
Mauricio Geraldi <145237881+mauriciogeraldi@users.noreply.github.com>
2025-01-04 22:03:02 +0100

    Update Principal.java

--- br.com.alura.conversor/Principal.java
+++ br.com.alura.conversor/Principal.java
@@ -19,23 +19,22 @@ import java.util.Scanner;
 public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        Scanner leitura = new Scanner(System.in); // Scanner para capturar a entrada do usuário
-        String opcao=""; // Variável para armazenar a escolha do usuário
+        Scanner leitura = new Scanner(System.in); 
+        String opcao=""; 
         String moedaInicial = "USD";
         double valorInicial = 1;
         String moedaFinal = "USD";
         double valorFinal = 1;
         String sair = "sair";
 
-        // Inicializa o objeto Gson com formatação personalizada
+ 
         Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE) // Define a política de nomeação de campos como CamelCase
-                .setPrettyPrinting() // Configura o Gson para imprimir o JSON de forma legível
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE) 
+                .setPrettyPrinting() 
                 .create();
 
         while (!opcao.equalsIgnoreCase(sair)) {
 
-            // Exibe o menu de opções ao usuário
             System.out.println("Seja bem vindo(a) ao Conversor de Moedas!");
             System.out.println("Digite a moeda inicial:");
             moedaInicial = leitura.nextLine();
@@ -45,31 +44,24 @@ public class Principal {
             valorInicial = leitura.nextDouble();
 
 
-            // URL da API de taxas de câmbio com chave de API
             String endereco = "https://v6.exchangerate-api.com/v6/4bd1f36289d5f1a9016fe246/latest/"+moedaInicial;
 
-            // Cria um cliente HTTP para enviar solicitações
             HttpClient client = HttpClient.newHttpClient();
 
-            // Cria uma solicitação HTTP para o endpoint da API
             HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco)) // Define a URI da API
+                    .uri(URI.create(endereco)) 
                     .build();
 
-            // Envia a solicitação e recebe a resposta em formato String
             HttpResponse<String> response = client
                     .send(request, HttpResponse.BodyHandlers.ofString());
 
-            // Armazena o corpo da resposta JSON em uma String
             String json = response.body();
 
-            // Converte o JSON recebido para o objeto `Moeda`
             Moeda moeda = gson.fromJson(
-                    gson.fromJson(json, JsonObject.class) // Converte a String JSON para um JsonObject
-                            .getAsJsonObject("conversion_rates") // Obtém o objeto "conversion_rates" que contém as taxas de câmbio
-                            .toString(), Moeda.class); // Converte as taxas de câmbio para o objeto `Moeda`
+                    gson.fromJson(json, JsonObject.class) 
+                            .getAsJsonObject("conversion_rates") 
+                            .toString(), Moeda.class); 
 
-            // Configura o formato decimal para exibir até 14 casas decimais
             DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.ENGLISH);
             DecimalFormat decimalFormat = new DecimalFormat("0.00", dfs);
 
